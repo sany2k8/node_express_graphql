42010d6fa21f8b2556a8679bbfd9c5f0e4a93f0e
sany <sany2k8@gmail.com>
2017-08-06 23:26:26 +0600

    Add server.js file with /graphql endpoint for hello world

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+type Query {
+    hello : String
+} 
+`);
+
+var root = {
+    hello: () =>
+    {  
+         return 'Hello World';
+    },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema : schema,
+    rootValue:root,
+    graphiql:true
+}));
+
+app.listen(4000);
+console.log('GraphQL is listening on localhost:4000/graphql');
